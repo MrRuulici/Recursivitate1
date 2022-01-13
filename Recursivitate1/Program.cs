a1015c9de21ce3aec864122f538f17c461b09812
Raul Ulici <raululici@yahoo.com>
2022-01-13 16:29:02 +0200

    final

---
+++ Recursivitate1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Recursivitate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[] { 5, 6, 7, 8, 9, 10 };
+            Recursivitate.Parcurgere(v, v.Length);
+        }
+    }
+}
---
+++ Recursivitate1/Recursivitate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursivitate1
+{
+    public static class Recursivitate
+    {
+        public static void Parcurgere(int[] v, int n)
+        {
+            if (n == 1)
+                Console.WriteLine(v[n-1]);
+            else 
+            {
+                Parcurgere(v, n - 1);
+                Console.WriteLine(v[n]);
+            }
+        }
+    }
+}
